68d95c930d2cc7c10c4f19cba46be17a844c4277
unknown <ericksespinoza1@gmail.com>
2022-07-06 11:07:38 -0500

    Agregado nuevo archivo, archivo-ejemplo.js

---
+++ archivo-ejemplo.js.js
@@ -0,0 +1,16 @@
+var hola = 1;
+var a = 1+1;
+
+function permisoedad (edad){
+if (edad > 18){
+    return true;
+}
+return false;
+}
+permisoedad (22);
+var numbres =[erick, marcos , victor];
+console.log (numbres.length);
+
+
+
+
